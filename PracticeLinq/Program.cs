8e952649280801eeac7262bb0a64cd861f9a872c
Stephen Wills <stephenrwills94@gmail.com>
2022-07-20 15:36:12 -0500

    finished main method

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+List<string> videoGames = new List<string>() { 
+
+    "Donkey Kong",
+    "Banjo Kazooie",
+    "Jade Empire",
+    "Fallout",
+    "Portal",
+    "Bloodborne"
+};
+
+videoGames.OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine(x));
+
+
+
